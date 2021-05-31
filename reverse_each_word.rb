d7d2842aa1877165ec8ef4b93e7a1b38ecb855f5
DJ Taylor <dtaylor88@gmail.com>
2021-05-31 05:31:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   split_array = string.split(" ")
   reverse_array = []
-  split.each do |word|
+  split_array.each do |word|
     word.reverse << reverse_array
   end
   reverse_array.join
